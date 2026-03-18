ab157a3cf50e5e002d5a465f65d601f7a5c99ec1
iwon77 <iwon77r@gmail.com>
2026-03-18 17:25:47 +0900

    chore: session에서 redis 연결 분리

--- backend/src/config/session.ts
+++ backend/src/config/session.ts
@@ -1,13 +1,6 @@
 import session from "express-session";
 import { RedisStore } from "connect-redis";
-import { createClient } from "redis";
-
-export const redisClient = createClient({ url: process.env.REDIS_URL });
-
-redisClient
-  .connect()
-  .then(() => console.log("Redis 연결 성공"))
-  .catch((err) => console.error("Redis 연결 실패:", err));
+import { redisClient } from "./redis"
 
 export const sessionMiddleware = session({
   store: new RedisStore({ client: redisClient }),
