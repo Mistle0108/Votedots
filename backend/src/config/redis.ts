82c83ff7945090e609bf4ea281a627193c52d3ca
iwon77 <iwon77r@gmail.com>
2026-03-18 17:31:17 +0900

    refactor: err.message -> err

--- backend/src/config/redis.ts
+++ backend/src/config/redis.ts
@@ -5,4 +5,4 @@ export const redisClient = createClient({ url: process.env.REDIS_URL })
 redisClient
   .connect()
   .then(() => console.log("Redis 연결 성공"))
-  .catch((err: Error) => console.error("Redis 연결 실패:", err.message))
+  .catch((err: Error) => console.error("Redis 연결 실패:", err))
