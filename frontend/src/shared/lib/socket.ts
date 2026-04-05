740721cbc3b6d6dd2c929f14b7c79bc4e7fa789f
iwon77 <iwon77r@gmail.com>
2026-04-05 17:46:37 +0900

    chore: split-config

--- frontend/src/shared/lib/socket.ts
+++ frontend/src/shared/lib/socket.ts
@@ -1,8 +1,9 @@
 import { io } from "socket.io-client";
 
-const socket = io(import.meta.env.VITE_API_URL ?? "http://localhost:4000", {
+const socket = io(import.meta.env.VITE_SOCKET_URL ?? "/", {
   withCredentials: true,
   autoConnect: false,
+  path: "/socket.io",
 });
 
 export default socket;
