b02ced0fd4106b82c698df324bc0bafdfb9bc87e
iwon77 <iwon77r@gmail.com>
2026-05-09 17:18:02 +0900

    feat: 프록시 경로 수정

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
       "/api": {
         target: "http://backend:4000",
         changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
       },
     },
   },
